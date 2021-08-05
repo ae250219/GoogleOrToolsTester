195b7c1d362f7b9a7f3139923959fa4b091e19d6
CORP\ae250219 <ae250219.ncr.com>
2021-08-05 11:27:53 +0300

    Add OrTool tester

---
+++ GoogleOrToolsTesterDocker/Controllers/SolverController.cs
@@ -0,0 +1,36 @@
+﻿using Google.OrTools.ConstraintSolver;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using static Google.OrTools.LinearSolver.Solver;
+
+namespace GoogleOrToolsTesterDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SolverController : ControllerBase
+    {
+
+        private readonly ILogger<SolverController> _logger;
+
+        public SolverController(ILogger<SolverController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var solver = new Solver("MyMax");
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogCritical(ex, "failed create solver");
+                throw;
+            }
+            return Ok("solver created");
+        }
+    }
+}
